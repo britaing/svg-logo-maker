ff68f01e08159014dd61bf1590c86bf5500c53c7
Britain Gilgour <britaing@gmail.com>
2023-08-21 12:13:23 -0500

    added inquire prompts and fs code

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateSVG = require('./lib/shapes');
 
-const questions = () =>
-{}
+const questions = () => inquirer.createPromptModule([
+    {
+        name: 'text',
+        type: 'input',
+        message: 'Please enter up to three characters'
+    },
+    {
+        name: 'textColor',
+        type: 'input',
+        message: 'In which color would you prefer the text to appear? You can input either a color keyword or a hexadecimal value.'
+    },
+    {
+        name: 'shape',
+        type: 'list',
+        choices: ['circle', 'triangle', 'square']
+    },
+    {
+        name: 'shapeColor',
+        type: '',
+        message: 'In which color would you prefer the shape to appear? You can input either a color keyword or a hexadecimal value.'
+    }
+]);
+
+questions().then((answers) => {
+    const generated = generateSVG(answers);
+    fs.writeFileSync('logo.svg', generated)
+}).then(() => console.log('Success! your svg file has been generated!'));
+
+function init(){}
+init();
